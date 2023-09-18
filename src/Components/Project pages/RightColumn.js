141833074f3dd099158f7fec6941899db80556c4
alaleeeli <144134693+alaleeeli@users.noreply.github.com>
2023-09-18 12:03:09 +0400

    removed files

--- src/Components/Project pages/RightColumn.js
+++
@@ -1,8 +0,0 @@
-export function RightColumn({ children }) {
-    return (
-        <div class="w-full md:w-2/3 text-left px-4">
-            <p class="text-2xl font-semibold"> About </p>
-            <p class=" mb-5 text-lg"> {children} </p>
-        </div>
-    );
-}
